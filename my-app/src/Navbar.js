3916fb6914de68755c7680c706b63350eec9a9fd
Joe D <joedeejr@gmail.com>
2022-02-01 11:22:06 -0300

    color correction

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,14 +1,10 @@
-
-
 function Navbar () {
 
     return (
         
     <div className= "Navbar">
-        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png
-            " alt="ironhack logo"/> 
-        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png
-        " alt="menu" /> 
+        <img src="./images/ironhack-logo.png" alt="ironhack logo"/> 
+        <img src="./images/menu-top.png" alt="menu" /> 
     </div>
 
     
